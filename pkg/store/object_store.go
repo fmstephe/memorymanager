9365b16f5a715ecdc257856b60911755166e54b2
Francis Stephens <francisstephens@gmail.com>
2022-10-22 18:39:48 +1300

    Objectstore.Free(ObjectPointer) method added
    This method allows us to 'free' an object, this object can then be
    reused for future Get() calls.
    
    To achieve this we added a very simple linked list wrapper to each
    object.
    
    The ObjectStore itself has a pointer to a free object (this pointer may
    be nil). This pointer is called the root free pointer, all freed objects
    are reachable starting from the root free pointer and following the
    linked list.
    
    When allocating a new object we first check to see if a
    previously freed object is available. If a freed object is available we
    return that and then set the root free pointer to point at the next
    object in the linked list (which might be nil).
    
    We maintain an important property. Any freed object (which has not been
    re-allocated) has a non-nil pointer to another freed object. In the case
    where there isn't another freed object to point to, the freed object
    points to itself.
    
    This allows us to test whether an object being accessed via Get is
    actually free. We can also test if an object being Free(ed) is already
    free. In both of these cases the method calls will panic.
    
    After writing all of this code, we seem to have a mixture of terminology
    developing. We Get() a new object and then increment a field called
    allocs. This starts to feel confusing. We should try to tidy up the
    naming of these fields and methods in the future.

--- pkg/store/object_store.go
+++ pkg/store/object_store.go
@@ -1,13 +1,25 @@
 package store
 
+import "fmt"
+
 const objectChunkSize = 1024
 
 type ObjectStore[O any] struct {
 	// Immutable fields
 	chunkSize int32
 
-	offset  int32
-	objects [][]O
+	// Accounting fields
+	allocs int
+	frees  int
+
+	offset   int32
+	nextFree ObjectPointer[O]
+	objects  [][]objectWrapper[O]
+}
+
+type objectWrapper[O any] struct {
+	nextFree ObjectPointer[O]
+	object   O
 }
 
 type ObjectPointer[O any] struct {
@@ -22,7 +34,7 @@ func (p ObjectPointer[O]) IsNil() bool {
 func NewObjectStore[O any]() *ObjectStore[O] {
 	chunkSize := int32(objectChunkSize)
 	// Initialise the first chunk
-	objects := [][]O{make([]O, chunkSize)}
+	objects := [][]objectWrapper[O]{make([]objectWrapper[O], chunkSize)}
 	return &ObjectStore[O]{
 		chunkSize: chunkSize,
 		offset:    0,
@@ -31,20 +43,81 @@ func NewObjectStore[O any]() *ObjectStore[O] {
 }
 
 func (s *ObjectStore[O]) New() (ObjectPointer[O], *O) {
+	s.allocs++
+
+	if s.nextFree.IsNil() {
+		return s.newFromOffset()
+	}
+
+	return s.newFromFree()
+}
+
+func (s *ObjectStore[O]) Get(p ObjectPointer[O]) *O {
+	wrapper := s.getWrapper(p)
+	if !wrapper.nextFree.IsNil() {
+		panic(fmt.Errorf("Attempted to Get freed object %v", p))
+	}
+	return &wrapper.object
+}
+
+func (s *ObjectStore[O]) Free(p ObjectPointer[O]) {
+	wrapper := s.getWrapper(p)
+
+	if !wrapper.nextFree.IsNil() {
+		panic(fmt.Errorf("Attempted to Free freed object %v", p))
+	}
+
+	s.frees++
+
+	if s.nextFree.IsNil() {
+		wrapper.nextFree = p
+	} else {
+		wrapper.nextFree = s.nextFree
+	}
+
+	s.nextFree = p
+}
+
+func (s *ObjectStore[O]) AllocCount() int {
+	return s.allocs
+}
+
+func (s *ObjectStore[O]) FreeCount() int {
+	return s.frees
+}
+
+func (s *ObjectStore[O]) LiveCount() int {
+	return s.allocs - s.frees
+}
+
+func (s *ObjectStore[O]) Chunks() int {
+	return len(s.objects)
+}
+
+func (s *ObjectStore[O]) newFromFree() (ObjectPointer[O], *O) {
+	oldFree := s.nextFree
+
+	freeWrapper := s.getWrapper(oldFree)
+	s.nextFree = freeWrapper.nextFree
+	freeWrapper.nextFree = ObjectPointer[O]{}
+	return oldFree, &freeWrapper.object
+}
+
+func (s *ObjectStore[O]) newFromOffset() (ObjectPointer[O], *O) {
 	chunk := int32(len(s.objects))
 	s.offset++
 	offset := s.offset
 	if s.offset == s.chunkSize {
 		// Create a new chunk
-		s.objects = append(s.objects, make([]O, s.chunkSize))
+		s.objects = append(s.objects, make([]objectWrapper[O], s.chunkSize))
 		s.offset = 0
 	}
 	return ObjectPointer[O]{
 		chunk:  chunk,
 		offset: offset,
-	}, &s.objects[chunk-1][offset-1]
+	}, &s.objects[chunk-1][offset-1].object
 }
 
-func (s *ObjectStore[O]) Get(p ObjectPointer[O]) *O {
+func (s *ObjectStore[O]) getWrapper(p ObjectPointer[O]) *objectWrapper[O] {
 	return &s.objects[p.chunk-1][p.offset-1]
 }
--- pkg/store/object_store_test.go
+++ pkg/store/object_store_test.go
@@ -25,6 +25,10 @@ func Test_Object_NewModifyGet(t *testing.T) {
 		pointers[i] = p
 	}
 
+	assert.Equal(t, len(pointers), os.AllocCount())
+	assert.Equal(t, len(pointers), os.LiveCount())
+	assert.Equal(t, 0, os.FreeCount())
+
 	// Assert that all of the modifications are visible
 	for i, p := range pointers {
 		s := os.Get(p)
@@ -46,6 +50,10 @@ func Test_Object_GetModifyGet(t *testing.T) {
 		pointers[i] = p
 	}
 
+	assert.Equal(t, len(pointers), os.AllocCount())
+	assert.Equal(t, len(pointers), os.LiveCount())
+	assert.Equal(t, 0, os.FreeCount())
+
 	// Get each object and modify field
 	for i, p := range pointers {
 		s := os.Get(p)
@@ -57,4 +65,76 @@ func Test_Object_GetModifyGet(t *testing.T) {
 		s := os.Get(p)
 		assert.Equal(t, i*2, s.Field)
 	}
+}
+
+// Demonstrate that we can create an object, then free it. If we try to Get()
+// the freed object ObjectStore panics
+func Test_Object_NewFreeGet_Panic(t *testing.T) {
+	os := NewObjectStore[MutableStruct]()
+	p, _ := os.New()
+	os.Free(p)
+
+	assert.Panics(t, func() { os.Get(p) })
+}
+
+// Demonstrate that we can create an object, then free it. If we try to Free()
+// the freed object ObjectStore panics
+func Test_Object_NewFreeFree_Panic(t *testing.T) {
+	os := NewObjectStore[MutableStruct]()
+	p, _ := os.New()
+	os.Free(p)
+
+	assert.Panics(t, func() { os.Free(p) })
+}
+
+// Demonstrate that if we create a large number of objects, then free them,
+// then allocate that same number again, we re-use the freed objects
+func Test_Object_NewFreeNew_ReusesOldObjects(t *testing.T) {
+	os := NewObjectStore[MutableStruct]()
+
+	objectAllocations := objectChunkSize * 3
+
+	// Create a large number of objects
+	pointers := make([]ObjectPointer[MutableStruct], objectChunkSize*3)
+	for i := range pointers {
+		p, _ := os.New()
+		pointers[i] = p
+	}
+
+	// We have allocate one batch of objects
+	assert.Equal(t, objectAllocations, os.AllocCount())
+	// They are all live
+	assert.Equal(t, objectAllocations, os.LiveCount())
+	// Nothing has been freed
+	assert.Equal(t, 0, os.FreeCount())
+	// Internally 4 chunks have been created
+	assert.Equal(t, 4, os.Chunks())
+
+	// Free all of those objects
+	for _, p := range pointers {
+		os.Free(p)
+	}
+
+	// We have allocate one batch of objects
+	assert.Equal(t, objectAllocations, os.AllocCount())
+	// None are live
+	assert.Equal(t, 0, os.LiveCount())
+	// We have freed one batch of objects
+	assert.Equal(t, objectAllocations, os.FreeCount())
+	// Internally 4 chunks have been created
+	assert.Equal(t, 4, os.Chunks())
+
+	// Allocate the same number of objects again
+	for range pointers {
+		os.New()
+	}
+
+	// We have allocated 2 batches of objects
+	assert.Equal(t, 2*objectAllocations, os.AllocCount())
+	// We have freed one batch
+	assert.Equal(t, objectAllocations, os.LiveCount())
+	// One batch is live
+	assert.Equal(t, objectAllocations, os.FreeCount())
+	// Internally 4 chunks have been created
+	assert.Equal(t, 4, os.Chunks())
 }
