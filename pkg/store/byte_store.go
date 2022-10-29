69b43e68d249c5b3d322bfd6c93dd8fb0718b81a
Francis Stephens <francisstephens@gmail.com>
2022-10-29 17:15:38 +1300

    ByteStore now allocates from pre-sized byte slabs
    These slabs are all power of 2 sized, although the first 4 bytes is used
    to record the size of the actual allocation. So for example, if you want
    to allocate 4 bytes, you will need at least an 8 byte sized slab to
    allow for the size to be written.
    
    This approach is adopted to make it easier to free allocated byte
    slices. For this to be reasonably simple we would rather allocate and
    free from pre-sized byte slices.
    
    The smallest slab used is 8 bytes, so 0, 1, 2, 3 and 4 byte allocations
    all use this smallest slab. The remaining slabs are simply successive
    powers of 2 in size, e.g. 8, 16, 32, 64, ...
    
    The hardest part of implementing this was building a reliable mapping
    from allocation size to an index starting at 0 (so we could lookup a
    slab by index using the desired allocation size). Some subtle code was
    developed to achieve this, there may be a simpler way.

--- pkg/store/byte_store.go
+++ pkg/store/byte_store.go
@@ -1,11 +1,15 @@
 package store
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+	"math/bits"
+)
 
 type BytePointer struct {
-	chunk  int32
-	offset int32
-	size   int32
+	chunk  uint32
+	offset uint32
+	size   uint32
 }
 
 func (p BytePointer) IsNil() bool {
@@ -13,58 +17,134 @@ func (p BytePointer) IsNil() bool {
 }
 
 type ByteStore struct {
+	slabs []byteSlab
+}
+
+func NewByteStore(_ int32) *ByteStore {
+	slabs := make([]byteSlab, 16)
+	totalSize := uint32(8)
+	for range slabs {
+		allocSize := totalSize - 4
+		idx := indexForSize(allocSize)
+		slabs[idx] = newByteSlab(allocSize, 2<<10)
+		totalSize *= 2
+	}
+
+	return &ByteStore{
+		slabs: slabs,
+	}
+}
+
+func (s *ByteStore) New(size uint32) (BytePointer, error) {
+	idx := indexForSize(size)
+	return s.slabs[idx].alloc(size)
+}
+
+func (s *ByteStore) Get(obj BytePointer) []byte {
+	idx := indexForSize(obj.size)
+	return s.slabs[idx].get(obj)
+}
+
+func indexForSize(size uint32) uint32 {
+	size += zeroToOne(size) // Use this to make 0 -> 1
+	slotSize := size + 4
+	count := uint32(bits.Len32(nextPowerOf2(slotSize)))
+	return count - 4
+}
+
+// This oddity converts zero to 1, other number are unaffected
+func zeroToOne(val uint32) uint32 {
+	val |= val >> 1
+	val = val &^ (1 << 31)
+	val = val - 1
+	val = val >> 31
+	return val
+}
+
+func nextPowerOf2(val uint32) uint32 {
+	// Check if val _is_ a power of 2
+	if val > 0 && val&(val-1) == 0 {
+		return val
+	}
+	return 1 << bits.Len32(val)
+}
+
+type byteSlab struct {
 	// Immutable fields
-	chunkSize int32
+	slotBits  uint32 // Debugging only
+	allocSize uint32
+	slotSize  uint32
+	slotBatch uint32
+	chunkSize uint32
 
 	// Mutable fields
-	offset int32
+	offset uint32
 	bytes  [][]byte
 }
 
 // Convenience constants to make it easier to declare large max object size values
 const (
-	_        = iota // ignore first value by assigning to blank identifier
-	KB int32 = 1 << (10 * iota)
+	_         = iota // ignore first value by assigning to blank identifier
+	KB uint32 = 1 << (10 * iota)
 	MB
 	GB
 )
 
-func NewByteStore(maxObjectSize int32) *ByteStore {
-	// Initialise bytes with a single empty chunk available
-	bytes := [][]byte{make([]byte, maxObjectSize)}
+func newByteSlab(allocSize, slotBatch uint32) byteSlab {
+	slotSize := allocSize + 4
+	chunkSize := slotSize * slotBatch
 
-	return &ByteStore{
-		chunkSize: maxObjectSize,
+	// Initialise bytes with a single empty chunk available
+	bytes := [][]byte{make([]byte, chunkSize)}
+
+	return byteSlab{
+		slotBits:  uint32(bits.Len32(slotSize)),
+		slotSize:  slotSize,
+		allocSize: allocSize,
+		slotBatch: slotBatch,
+		chunkSize: chunkSize,
 		offset:    0,
 		bytes:     bytes,
 	}
 }
 
-func (s *ByteStore) New(size int32) (BytePointer, error) {
-	if size > s.chunkSize {
-		return BytePointer{}, fmt.Errorf("data of size %d, is greater than object size limit %d", size, s.chunkSize)
+func (s *byteSlab) alloc(size uint32) (BytePointer, error) {
+	if size > s.allocSize {
+		panic(fmt.Errorf("Bad alloc size, max allowed %d, %d was requested", s.slotSize-4, size))
 	}
-
-	// If current chunk is too small, create a new chunk and use that
-	if s.offset+size > s.chunkSize {
+	// If we have used up the last chunk create a new one
+	if s.offset == s.chunkSize {
 		s.offset = 0
 		s.bytes = append(s.bytes, make([]byte, s.chunkSize))
 	}
 
+	s.writeSize(uint32(len(s.bytes))-1, s.offset, size)
+
 	// Create BytePointer pointing to the new slice
 	obj := BytePointer{
-		chunk:  int32(len(s.bytes)),
+		chunk:  uint32(len(s.bytes)),
 		offset: s.offset + 1,
 		size:   size,
 	}
 
 	// Update offset
-	s.offset += size
+	s.offset += s.chunkSize
 
 	return obj, nil
 }
 
-func (s *ByteStore) Get(obj BytePointer) []byte {
+func (s *byteSlab) writeSize(chunk, offset, size uint32) {
+	bytes := s.bytes[chunk][offset : offset+s.slotSize]
+	binary.LittleEndian.PutUint32(bytes, uint32(size))
+}
+
+func (s *byteSlab) read(chunk, offset uint32) []byte {
+	bytes := s.bytes[chunk][offset : offset+s.slotSize]
+	size := binary.LittleEndian.Uint32(bytes)
+	return bytes[4 : 4+size]
+}
+
+func (s *byteSlab) get(obj BytePointer) []byte {
 	// There are no pre-checks here - if you pass in a malformed BytePointer
 	// this method may return nonsense or just panic
 
@@ -74,8 +154,6 @@ func (s *ByteStore) Get(obj BytePointer) []byte {
 	chunk := obj.chunk - 1
 	offset := obj.offset - 1
 
-	// Grab the actual byte values.  It's worth noting here that a pointer
-	// can have size 0, in this case a zero length slice is returned
-	bytes := s.bytes[chunk][offset : offset+obj.size]
+	bytes := s.read(chunk, offset)
 	return bytes
 }
--- pkg/store/byte_store_test.go
+++ pkg/store/byte_store_test.go
@@ -8,10 +8,79 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// Assert we can generate reliable byteSlab indexes for all sizes of
+// allocation Once we have this size->index mapping we can build separate
+// byteSlabs for each allocation size range.
+func TestIndexForSize(t *testing.T) {
+	// Zero is a special case - we test it specially
+	assert.Equal(t, uint32(0), indexForSize(0))
+
+	for i := uint32(1); i <= 4; i++ {
+		assert.Equal(t, uint32(0), indexForSize(i))
+	}
+
+	for i := uint32(5); i <= 12; i++ {
+		assert.Equal(t, uint32(1), indexForSize(i))
+	}
+
+	for i := uint32(13); i <= 28; i++ {
+		assert.Equal(t, uint32(2), indexForSize(i))
+	}
+
+	for i := uint32(29); i <= 60; i++ {
+		assert.Equal(t, uint32(3), indexForSize(i))
+	}
+
+	for i := uint32(61); i <= 124; i++ {
+		assert.Equal(t, uint32(4), indexForSize(i))
+	}
+
+	for i := uint32(125); i <= 252; i++ {
+		assert.Equal(t, uint32(5), indexForSize(i))
+	}
+
+	for i := uint32(253); i <= 508; i++ {
+		assert.Equal(t, uint32(6), indexForSize(i))
+	}
+
+	for i := uint32(509); i <= 1020; i++ {
+		assert.Equal(t, uint32(7), indexForSize(i))
+	}
+
+	// Hopefully these are enough cases, it's hard to write a generic
+	// test without just writing out the indexForSize method again
+}
+
+// To make the 0->4 allocations sizes all point to the same byteSlab we had to
+// convert 0 sized allocations to 1 sized allocations when deriving the index.
+// This is a pretty horrific, method as it was implemented without branching.
+// One could argue this was important for performance, but the truth is, it was
+// a fun challenge (and surprisingly hard)
+func TestZeroToOne(t *testing.T) {
+	// Assert that 0 converts to 1
+	assert.Equal(t, uint32(1), zeroToOne(0))
+
+	// Assert that some very low values convert to 0
+	// Assert that some very high values convert to 0
+	for value := uint32(1); value < 32; value++ {
+		assert.Equal(t, uint32(0), zeroToOne(value))
+		assert.Equal(t, uint32(0), zeroToOne(0-value))
+	}
+
+	// Assert that larger values around power of 2 thresholds convert to 0
+	// There's a bit of overlap with the previous loop - but we're ok with that
+	for shift := 1; shift < 32; shift++ {
+		value := uint32(1) << shift
+		assert.Equal(t, uint32(0), zeroToOne(value))
+		assert.Equal(t, uint32(0), zeroToOne(value+1))
+		assert.Equal(t, uint32(0), zeroToOne(value-1))
+	}
+}
+
 // Demonstrate that we can create bytes, then get those bytes and modify them
-// we can then get those bytes again and will see the modification
-// We ensure that we allocate so many bytes that we will need more than one chunk
-// to store all bytes.
+// we can then get those bytes again and will see the modification We ensure
+// that we allocate so many bytes that we will need more than one chunk to
+// store all bytes.
 func Test_Bytes_GetModifyGet(t *testing.T) {
 	const chunkSize = 1024
 	bs := NewByteStore(chunkSize)
@@ -40,16 +109,16 @@ func Test_Bytes_GetModifyGet(t *testing.T) {
 }
 
 // Demonstrate that we can create bytes, then get those bytes and modify them
-// we can then get those bytes again and will see the modification
-// We ensure that we allocate so many bytes that we will need more than one chunk
-// to store all bytes.
+// we can then get those bytes again and will see the modification.  We ensure
+// that we allocate so many bytes that we will need more than one chunk to
+// store all bytes.
 func Test_Bytes_GetModifyGet_OddSizing(t *testing.T) {
 	const chunkSize = 1024
 	bs := NewByteStore(chunkSize)
 
 	// Create all the byte slices
 	pointers := make([]BytePointer, chunkSize*3)
-	size := int32(0)
+	size := uint32(0)
 	for i := range pointers {
 		p, err := bs.New(size)
 		size++
