4ef733c511d721b6dc36fc9858635e3a9960918d
Francis Stephens <francisstephens@gmail.com>
2024-03-09 17:09:36 +1300

    Consume fuzz bytes with utility function
    We found that consuming bytes to produce values like uint32 or [16]byte
    was repetitive and error prone. Moved to a utility function which could
    be resused. This could be expanded to consume bytes and produce more
    types in the future.

--- pkg/store/objectstore/fuzz/fuzz_util.go
+++ pkg/store/objectstore/fuzz/fuzz_util.go
@@ -16,22 +16,30 @@ type Step interface {
 type Objects struct {
 	store    *objectstore.Store[[16]byte]
 	pointers []objectstore.Pointer[[16]byte]
-	objects  []*[16]byte
 	expected [][16]byte
 	// Indicates whether a pointer/object is still live (has not been freed)
 	live []bool
 }
 
-func (o *Objects) Add(value []byte) int {
+func (o *Objects) Alloc(value [16]byte) int {
 	ptr, obj := o.store.Alloc()
 	expected := [16]byte{}
-	copy(obj[:], value)
-	copy(expected[:], value)
+	copy(obj, value)
+	copy(expected, value)
 	o.pointers = append(o.pointers, ptr)
-	o.objects = append(o.objects, obj)
 	o.expected = append(o.expected, expected)
 	o.live = append(o.live, true)
 	return len(o.pointers) - 1
+}
+
+func (o *Objects) Mutate(index uint32, value [16]byte) {
+	// Update the allocated data
+	ptr := o.pointers[index]
+	obj := o.store.Get(ptr)
+	copy(obj, value)
+
+	// Update the expected
+	copy(o.expected, value)
 }
 
 func (o *Objects) Free(index uint32) {
@@ -57,52 +65,83 @@ func (o *Objects) Free(index uint32) {
 // Allocate an object
 type AllocStep struct {
 	objects *Objects
-	value   []byte
+	value   [16]byte
+	consumed []byte
 }
 
-func (s *AllocStep) ConsumeBytes(bytes []byte) []byte {
-	s.value = make([]byte, 16)
-	if len(bytes) <= 16 {
-		copy(s.value, bytes)
-		bytes = bytes[:0]
-	} else {
-		copy(s.value, bytes)
-		bytes = bytes[16:]
-	}
+func (s *AllocStep) ConsumeBytes(bytes []byte) (remaining []byte) {
+	s.consumed, remaining = consumeUpTo(bytes, 16)
+	copy(s.value, s.consumed)
 	return bytes
 }
 
 func (s *AllocStep) ProduceBytes() []byte {
-	return append([]byte{}, s.value...)
+	return consumed
 }
 
 func (s *AllocStep) DoStep() {
-	s.objects.Add(s.value)
+	s.objects.Alloc(s.value)
 }
 
 // Free an object
 type FreeStep struct {
 	objects *Objects
 	index   uint32
+	consumed []byte
 }
 
-func (s *FreeStep) ConsumeBytes(bytes []byte) []byte {
-	value := make([]byte, 4)
-	if len(bytes) < 4 {
-		copy(value, bytes)
-		bytes = bytes[:0]
-	} else {
-		copy(value, bytes[:4])
-		bytes = bytes[4:]
-	}
-	s.index = binary.LittleEndian.Uint32(value)
-	return bytes
+func (s *FreeStep) ConsumeBytes(bytes []byte) (remaining []byte) {
+	s.index, s.consumed, remaining = consumeUpTo(bytes, 4)
+	return remaining
 }
 
 func (s *FreeStep) ProduceBytes() []byte {
-	return binary.LittleEndian.AppendUint32([]byte{}, s.index)
+	return consumed
 }
 
 func (s *FreeStep) DoStep() {
 	s.objects.Free(s.index)
+}
+
+type MutateStep struct {
+	objects *Objects
+	index uint32
+	newValue [16]int
+	consumed []byte
+}
+
+func (s *MutateStep) ConsumeBytes(bytes []byte) (remaining []byte) {
+	s.consumed, remaining = consumeUpTo(bytes, 16)
+	copy(s.value, s.consumed)
+	return remaining
+}
+
+func (s *MutateStep) ProduceBytes() []byte {
+	return s.consumed
+}
+
+func (s *MutateStep) DoStep() {
+	s.objects.Mutate(s.index, s.newValue)
+}
+
+func consumeUpTo(bytes []byte, limit int) (consumed, remaining []byte) {
+	if len(bytes) <= limit {
+		consumed = make([]byte, len(bytes))
+		copy(s.consumed, bytes)
+		return consumed, bytes[:0]
+	}
+	consumed = make([]byte, limit)
+	copy(s.consumed, bytes)
+	return consumed, bytes[limit:]
+}
+
+func consumeUint32(bytes []byte) (value uint32, consumed, remaining []byte) {
+	consumed, remaining = consumeUpTo(bytes, 4)
+
+	// Build a 4 byte version of consumed to create the index
+	value := make([]byte, 4)
+	copy(value, consumed)
+	value = binary.LittleEndian.Uint32(value)
+
+	return value, consumed, remaining
 }
