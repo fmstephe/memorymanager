a085cb192f5027670b59f854c35a32c24912f9b4
Francis Stephens <francisstephens@gmail.com>
2024-09-01 19:38:26 +1200

    Adding interner for interning int64 values
    To achieve this we created a ConverterWithInt64Id called Int64Converter
    and an wrapper interner called Int64Interner.
    
    The resulting interface is very simple.
    
    type Int64Interner struct {
    	...
    }
    
    func (i *Int64Interner) Get(value int64) string {
    	...
    }
    
    func (i *Int64Interner) GetStats() StatsSummary {
    	...
    }

---
+++ pkg/store/intern/int64_interner.go
@@ -0,0 +1,48 @@
+package intern
+
+import (
+	"strconv"
+)
+
+type Int64Interner struct {
+	interner InternerWithUint64Id[Int64Converter]
+	base     int
+}
+
+func NewInt64Interner(maxLen, maxBytes, base int) *Int64Interner {
+	return &Int64Interner{
+		interner: NewInternerWithUint64Id[Int64Converter](maxLen, maxBytes),
+		base:     base,
+	}
+}
+
+func (i *Int64Interner) Get(value int64) string {
+	return i.interner.Get(NewInt64Converter(value, i.base))
+}
+
+func (i *Int64Interner) GetStats() StatsSummary {
+	return i.interner.GetStats()
+}
+
+var _ ConverterWithUint64Id = Int64Converter{}
+
+// A converter for int64 values. Here the identity is just the value itself.
+type Int64Converter struct {
+	value int64
+	base  int
+}
+
+func NewInt64Converter(value int64, base int) Int64Converter {
+	return Int64Converter{
+		value: value,
+		base:  base,
+	}
+}
+
+func (c Int64Converter) Identity() uint64 {
+	return uint64(c.value)
+}
+
+func (c Int64Converter) String() string {
+	return strconv.FormatInt(c.value, c.base)
+}
---
+++ pkg/store/intern/int64_interner_test.go
@@ -0,0 +1,202 @@
+package intern
+
+import (
+	"strconv"
+	"testing"
+	"unsafe"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInt64Interner_Interned(t *testing.T) {
+	interner := NewInt64Interner(64, 1024, 10)
+
+	// A string is returned with the same value as intVal
+	intVal := int64(1234)
+	internedInt := interner.Get(intVal)
+	assert.Equal(t, strconv.FormatInt(intVal, 10), internedInt)
+
+	// a new int value has been interned
+	expectedStats := Stats{Interned: 1}
+	stats := interner.GetStats()
+	assert.Equal(t, expectedStats, stats.Total)
+
+	// A string is returned with the same value as intVal
+	internedInt2 := interner.Get(intVal)
+	assert.Equal(t, strconv.FormatInt(intVal, 10), internedInt2)
+	// The string returned uses the same memory allocation as the first
+	// value returned i.e. the string is interned as is being reused as
+	// intended
+	assert.Equal(t, unsafe.StringData(internedInt), unsafe.StringData(internedInt2))
+
+	// An interned string has been returned
+	expectedStats = Stats{Interned: 1, Returned: 1}
+	stats = interner.GetStats()
+	assert.Equal(t, expectedStats, stats.Total)
+}
+
+func TestInt64Interner_NotInternedMaxLen(t *testing.T) {
+	interner := NewInt64Interner(3, 1024, 10)
+
+	// A string is returned with the same value as intVal
+	intVal := int64(1234)
+	notInternedInt := interner.Get(intVal)
+	assert.Equal(t, strconv.FormatInt(intVal, 10), notInternedInt)
+
+	// The int passed in was too long, so maxLenExceeded should be recorded
+	expectedStats := Stats{MaxLenExceeded: 1}
+	stats := interner.GetStats()
+	assert.Equal(t, expectedStats, stats.Total)
+
+	// A string is returned with the same value as intVal
+	notInternedInt2 := interner.Get(intVal)
+	assert.Equal(t, strconv.FormatInt(intVal, 10), notInternedInt2)
+	// The string returned uses a different memory allocation from the
+	// first value returned i.e. the strings were not interned, and a new
+	// string is being allocated each time
+	assert.NotSame(t, unsafe.StringData(notInternedInt), unsafe.StringData(notInternedInt2))
+
+	// The int passed in was too long, so maxLenExceeded should be recorded
+	expectedStats = Stats{MaxLenExceeded: 2}
+	stats = interner.GetStats()
+	assert.Equal(t, expectedStats, stats.Total)
+}
+
+func TestInt64Interner_NotInternedUsedInt(t *testing.T) {
+	interner := NewInt64Interner(64, 3, 10)
+
+	// A string is returned with the same value as intVal
+	intVal := int64(1234)
+	notInternedInt := interner.Get(intVal)
+	assert.Equal(t, strconv.FormatInt(intVal, 10), notInternedInt)
+
+	// The int passed in was too long, so usedBytesExceeded should be recorded
+	expectedStats := Stats{UsedBytesExceeded: 1}
+	stats := interner.GetStats()
+	assert.Equal(t, expectedStats, stats.Total)
+
+	// A string is returned with the same value as intVal
+	notInternedInt2 := interner.Get(intVal)
+	assert.Equal(t, strconv.FormatInt(intVal, 10), notInternedInt2)
+	// The string returned uses a different memory allocation from the
+	// first value returned i.e. the strings were not interned, and a new
+	// string is being allocated each time
+	assert.NotSame(t, unsafe.StringData(notInternedInt), unsafe.StringData(notInternedInt2))
+
+	// The int passed in was too long, so usedBytesExceeded should be recorded
+	expectedStats = Stats{UsedBytesExceeded: 2}
+	stats = interner.GetStats()
+	assert.Equal(t, expectedStats, stats.Total)
+}
+
+// This test demonstrates that the interner can handle passing through a
+// variety of states successfully. Specifically interning new ints, then
+// returning those as strings, then running out of usedBytes but continuing to
+// return previously interned int values.
+func TestInt64Interner_Complex(t *testing.T) {
+	interner := NewInt64Interner(1024, 1024, 10)
+	numberOfInts := 100
+
+	// When we intern all these ints, each one is unique and is interned
+	{
+		for intVal := range numberOfInts {
+			intVal64 := int64(intVal)
+			internedInt := interner.Get(intVal64)
+			assert.Equal(t, strconv.FormatInt(intVal64, 10), internedInt)
+		}
+
+		expectedStats := Stats{
+			Interned: numberOfInts,
+		}
+		stats := interner.GetStats()
+		assert.Equal(t, expectedStats, stats.Total)
+	}
+
+	// When we attempt to intern these ints again, they are already
+	// interned and their interned values are returned to us
+	{
+		for intVal := range numberOfInts {
+			intVal64 := int64(intVal)
+			internedInt := interner.Get(intVal64)
+			assert.Equal(t, strconv.FormatInt(intVal64, 10), internedInt)
+		}
+
+		expectedStats := Stats{
+			Interned: numberOfInts,
+			Returned: numberOfInts,
+		}
+		stats := interner.GetStats()
+		assert.Equal(t, expectedStats, stats.Total)
+	}
+
+	{
+		// This hack pushes up the recorded used-bytes to the limit.
+		// This means no other strings can be interned from here.
+		interner.interner.controller.usedBytes.Store(1024 * 1024)
+	}
+
+	// When we attempt to intern new floats there aren't enough bytes left
+	// to intern any of them
+	{
+		for intVal := range numberOfInts {
+			intVal64 := int64(intVal + numberOfInts)
+			internedInt := interner.Get(intVal64)
+			assert.Equal(t, strconv.FormatInt(intVal64, 10), internedInt)
+		}
+
+		expectedStats := Stats{
+			Interned:          numberOfInts,
+			Returned:          numberOfInts,
+			UsedBytesExceeded: numberOfInts,
+		}
+		stats := interner.GetStats()
+		assert.Equal(t, expectedStats, stats.Total)
+	}
+
+	// Finally we attempt to intern the ints again, they are already
+	// interned and their interned values are returned to us
+	{
+		for intVal := range numberOfInts {
+			intVal64 := int64(intVal)
+			internedInt := interner.Get(intVal64)
+			assert.Equal(t, strconv.FormatInt(intVal64, 10), internedInt)
+		}
+
+		expectedStats := Stats{
+			Interned:          numberOfInts,
+			Returned:          numberOfInts * 2,
+			UsedBytesExceeded: numberOfInts,
+		}
+		stats := interner.GetStats()
+		assert.Equal(t, expectedStats, stats.Total)
+	}
+}
+
+// This benchmark is intended to demonstrate that getting string values for
+// integers that have already been interned does not allocate
+func BenchmarkInt64Interner(b *testing.B) {
+	interner := NewInt64Interner(0, 0, 10)
+
+	ints := make([]int64, 10_000)
+	for i := range ints {
+		ints[i] = int64(i)
+	}
+
+	for _, intVal := range ints {
+		interner.Get(intVal)
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	count := 0
+	for {
+		for _, intVal := range ints {
+			interner.Get(intVal)
+			count++
+			if count >= b.N {
+				return
+			}
+		}
+	}
+}
