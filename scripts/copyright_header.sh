533eeba89e834923b718cf2a8da4950d119fbd34
Francis Stephens <francisstephens@gmail.com>
2024-09-09 17:44:46 +1200

    Add better copyright/license header
    The previous version had no whitespace after the copyright/license
    header. This meant that godoc would display the header once for every
    file in a package, which looks absurd.

---
+++ scripts/copyright_header.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+HEADER="// Copyright 2024 Francis Michael Stephens. All rights reserved.  Use of this
+// source code is governed by an MIT license that can be found in the LICENSE
+// file.
+"
+
+echo "$HEADER" > copyright-file
+
+for file in $(find . -type f -name \*.go -not -path "./vendor/*"); do
+	echo $file
+	line=$(head -n 1 $file)
+	if [[ $line != "// Copyright"* ]]; then
+		echo "$HEADER" > copyright-file
+		cat $file >> copyright-file
+		mv copyright-file $file
+	fi
+done
+
+rm copyright-file
