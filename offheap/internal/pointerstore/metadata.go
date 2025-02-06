c128b4f1a20120f6e55725ccb4710781da4a5bf6
Francis Stephens <francisstephens@gmail.com>
2025-02-06 20:11:16 +1300

    Update metadata comments
    Clarify that metadata contains both the generation _and_ the is-free
    tag, which are used to check the integrity of RefPointer data accesses.
    
    Writing these comments it strikes me that metadata is responsible for
    the integrity checks when accessing data through Refpointers, but the
    check themselves are done inside RefPointer methods. It seems like
    moving this code into metadata will improve code clarity.

--- offheap/internal/pointerstore/metadata.go
+++ offheap/internal/pointerstore/metadata.go
@@ -4,13 +4,14 @@
 
 package pointerstore
 
-// If the object's metadata has a non-nil nextFree pointer then the object is
-// currently free. Object's which have never been allocated are implicitly
-// free, but have a nil nextFree.
-//
 // A RefPointer smuggles a generation tag. Only references with the same gen
 // value can access/free objects they point to. This is a best-effort safety
 // check to try to catch use-after-free type errors.
+//
+// Metadata smuggles a generation tag and an is-free tag into its
+// taggedAddress. When accessing the data from a RefPointer we check that the
+// metadata indicates the object is not free and that the RefPointer's
+// generation matches that of the metadat.
 type metadata struct {
 	dataAddressAndGen taggedAddress
 }
